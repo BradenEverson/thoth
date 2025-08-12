16838a2fe6148c46c101de4f53d85e5f3de57a96
Braden Everson <bradeneverson@gmail.com>
2025-08-12 09:33:41 -0500

    Update build.zig and build.zig.zon

--- build.zig
+++ build.zig
@@ -1,8 +1,11 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
     const exe = b.addExecutable(.{
-        .name = "NAME",
+        .name = "thoth_sample",
         .root_source_file = b.path("src/main.zig"),
         .target = b.graph.host,
     });
@@ -12,10 +15,24 @@ pub fn build(b: *std.Build) void {
     const run_cmd = b.addRunArtifact(exe);
     run_cmd.step.dependOn(b.getInstallStep());
 
-    const run_step = b.step("run", "Put a better description here");
+    const run_step = b.step("run", "Run the sample program");
     if (b.args) |args| {
         run_cmd.addArgs(args);
     }
 
     run_step.dependOn(&run_cmd.step);
+
+    const thoth_mod = b.addModule("thoth", .{
+        .root_source_file = b.path("src/thoth.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const unit_tests = b.addTest(.{
+        .root_module = thoth_mod,
+    });
+
+    const run_unit_tests = b.addRunArtifact(unit_tests);
+    const test_step = b.step("test", "Run the unit tests");
+    test_step.dependOn(&run_unit_tests.step);
 }
