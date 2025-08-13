d96a09c13ba1549ded698254f564d3bc063bce83
BradenEverson <bradeneverson@gmail.com>
2025-08-13 11:43:35 -0500

    Create basic structure - still contemplating how I'm gonna do this

--- src/main.zig
+++ src/main.zig
@@ -1,3 +1,14 @@
 //! Sample runtime usage
 
-pub fn main() void {}
+const std = @import("std");
+const ThothScheduler = @import("thoth.zig");
+
+pub fn main() void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+
+    const alloc = gpa.allocator();
+
+    var scheduler = ThothScheduler.init(alloc);
+    defer scheduler.deinit();
+}
--- src/thoth.zig
+++ src/thoth.zig
@@ -1,2 +1,19 @@
 //! Async runtime struct
 
+const std = @import("std");
+const Task = @import("task.zig");
+const Allocator = std.mem.Allocator;
+
+allocator: Allocator,
+curr: usize,
+queue: std.ArrayList(Task),
+
+const Self = @This();
+
+pub fn init(allocator: Allocator) Self {
+    return Self{ .allocator = allocator, .curr = 0, .queue = std.ArrayList(Task).init(allocator) };
+}
+
+pub fn deinit(self: *Self) void {
+    self.queue.deinit();
+}
