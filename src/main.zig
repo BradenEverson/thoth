3ec004415f0a726a5be9e51fc5438afff8a2363b
BradenEverson <bradeneverson@gmail.com>
2025-08-13 12:43:36 -0500

    Create a signal that fires every s, this will be the root of the time delta allowed for each "process"

--- src/main.zig
+++ src/main.zig
@@ -3,7 +3,12 @@
 const std = @import("std");
 const ThothScheduler = @import("thoth.zig");
 
-pub fn main() void {
+pub fn timerHandler(_: i32) callconv(.C) void {
+    std.debug.print("Signal received\n", .{});
+    _ = alarm(1);
+}
+
+pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
 
@@ -11,4 +16,22 @@ pub fn main() void {
 
     var scheduler = ThothScheduler.init(alloc);
     defer scheduler.deinit();
+
+    var action: std.os.linux.Sigaction = undefined;
+    action.flags = 0;
+    action.mask = std.os.linux.empty_sigset;
+    action.handler.sigaction = null;
+    action.handler.handler = timerHandler;
+
+    _ = std.os.linux.sigaction(std.os.linux.SIG.ALRM, &action, null);
+
+    _ = alarm(1);
+    while (true) {}
+}
+
+fn alarm(seconds: usize) usize {
+    return std.os.linux.syscall1(
+        .alarm,
+        seconds,
+    );
 }
