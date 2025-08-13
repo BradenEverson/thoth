6447e863dfc270fccc5b7ffee0d07d3215d0a0b2
BradenEverson <bradeneverson@gmail.com>
2025-08-13 13:38:28 -0500

    Make the struct prettier and just do a little messin around

--- src/main.zig
+++ src/main.zig
@@ -8,9 +8,10 @@ const TIME_QUANTUM: comptime_int = 10_000;
 const US_PER_S: comptime_int = 1_000_000;
 
 var scheduler: ?ThothScheduler = null;
+var i: u128 = 0;
 
 pub fn timerHandler(_: i32) callconv(.C) void {
-    std.debug.print("Signal received\n", .{});
+    i += 1;
 }
 
 pub fn main() !void {
@@ -30,14 +31,20 @@ pub fn main() !void {
 
     _ = std.os.linux.sigaction(std.os.linux.SIG.ALRM, &action, null);
 
-    var spec: std.os.linux.itimerspec = undefined;
-    spec.it_value.sec = TIME_QUANTUM / US_PER_S;
-    spec.it_value.nsec = TIME_QUANTUM % US_PER_S;
-
-    spec.it_interval.sec = TIME_QUANTUM / US_PER_S;
-    spec.it_interval.nsec = TIME_QUANTUM % US_PER_S;
+    var spec: std.os.linux.itimerspec = .{
+        .it_value = .{
+            .sec = TIME_QUANTUM / US_PER_S,
+            .nsec = TIME_QUANTUM % US_PER_S,
+        },
+        .it_interval = .{
+            .sec = TIME_QUANTUM / US_PER_S,
+            .nsec = TIME_QUANTUM % US_PER_S,
+        },
+    };
 
     _ = std.os.linux.setitimer(@intFromEnum(std.os.linux.ITIMER.REAL), &spec, null);
 
-    while (true) {}
+    while (true) {
+        std.debug.print("{}\n", .{i});
+    }
 }
