2846d62d89313116043be936a69e0b0f1a245bdf
BradenEverson <bradeneverson@gmail.com>
2025-08-18 07:51:42 -0500

    Catch unsuported architectures better

--- src/thoth.zig
+++ src/thoth.zig
@@ -25,7 +25,7 @@ pub fn ThothScheduler(comptime max_tasks: u32, comptime stack_size: u32) type {
         pub const Context = switch (builtin.cpu.arch) {
             .x86_64 => X86_64Context(stack_size),
             .arm => ARMContext(stack_size),
-            else => @compileError("Unsupported CPU architecture"),
+            else => @compileError("Unsupported CPU architecture: " ++ @tagName(builtin.cpu.arch)),
         };
 
         const Self = @This();
